526a30439a9f7c1de133803e822c6f0bc1bbb01c
Jayesh Salvi <jayeshsalvi@gmail.com>
2010-07-21 10:28:19 +0530

    CLI utility "dmeter" to view the collected stats.

--- src/database.py
+++ src/database.py
@@ -2,6 +2,12 @@
 import sqlite3
 import os
 
+class Interval:
+    def __init__(self, start, end, idle, app):
+        self.start = start
+        self.end = end
+        self.idle = idle
+        self.app = app
 
 class Database:
     def __init__(self):
@@ -29,4 +35,33 @@ class Database:
         '''%(start, end, isIdle, appname))
         self.conn.commit()
         c.close()
+
+    def get_intervals(self, where_clause, args=[], distinct=None):
+        c = self.conn.cursor()
+
+        if distinct is not None:
+            result = c.execute('select distinct(%s) from intervals where '%distinct+where_clause, args)
+            intervals = []
+            for row in result:
+                intervals.append(row[0])
+        else:
+            result = c.execute('select * from intervals where '+where_clause, args)
+            intervals = []
+            for row in result:
+                intervals.append(Interval(row[0], row[1], row[2], row[3]))
+
+
+        c.close()
+
+        return intervals
+
+    def count_intervals(self, where_clause, args=[]):
+        c = self.conn.cursor()
+
+        c.execute('select count(*) from intervals where '+where_clause, args)
+        result = c.fetchone()
+        c.close()
+
+        return result[0]
+
 
